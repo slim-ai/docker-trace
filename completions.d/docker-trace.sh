1d020b6bd3fc16ef697b1cbe02be6a3962aa83c1
nathants <me@nathants.com>
2021-09-19 20:49:46 -1000

    completion should support zsh

--- completions.d/docker-trace.sh
+++ completions.d/docker-trace.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -z "$ZSH_NAME" ]; then
+    autoload bashcompinit
+    bashcompinit
+fi
+
 _docker_trace () {
     if [ $COMP_CWORD = 1 ]; then
 	    COMPREPLY=($(docker-trace -h 2>/dev/null | awk '{print $1}' | grep "^${COMP_WORDS[$COMP_CWORD]}"))
